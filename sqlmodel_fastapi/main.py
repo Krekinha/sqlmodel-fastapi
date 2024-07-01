cc7e26f6c0aae3298461d523d8a15a949dc35782
Krekinha <krekmg@gmail.com>
2024-07-01 06:21:45 -0300

    06:21

--- sqlmodel_fastapi/main.py
+++ sqlmodel_fastapi/main.py
@@ -1,5 +1,36 @@
-from fastapi import FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import (
+    Field,
+    Relationship,
+    Session,
+    SQLModel,
+    create_engine,
+    select,
+)
+
+
+class TeamBase(SQLModel):
+    name: str = Field(index=True)
+    headquarters: str
+
+
+class Team(TeamBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    heroes: list["Hero"] = Relationship(back_populates="team")
+
+
+class TeamCreate(TeamBase):
+    pass
+
+
+class TeamPublic(TeamBase):
+    id: int
+
+
+class TeamUpdate(SQLModel):
+    name: str | None = None
+    headquarters: str | None = None
 
 
 class HeroBase(SQLModel):
@@ -7,10 +38,14 @@ class HeroBase(SQLModel):
     secret_name: str
     age: int | None = Field(default=None, index=True)
 
+    team_id: int | None = Field(default=None, foreign_key="team.id")
+
 
 class Hero(HeroBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     hashed_password: str = Field()
+
+    team: Team | None = Relationship(back_populates="heroes")
 
 
 class HeroCreate(HeroBase):
@@ -26,6 +61,15 @@ class HeroUpdate(SQLModel):
     secret_name: str | None = None
     age: int | None = None
     password: str | None = None
+    team_id: int | None = None
+
+
+class HeroPublicWithTeam(HeroPublic):
+    team: TeamPublic | None = None
+
+
+class TeamPublicWithHeroes(TeamPublic):
+    heroes: list[HeroPublic] = []
 
 
 sqlite_file_name = "database.db"
@@ -44,6 +88,11 @@ def hash_password(password: str) -> str:
     return f"not really hashed {password} hehehe"
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 app = FastAPI()
 
 
@@ -53,63 +102,145 @@ async def on_startup():
 
 
 @app.post("/heroes/", response_model=HeroPublic)
-def create_hero(hero: HeroCreate):
+# como o parâmetro session tem um valor padrão, usamos o parâmetro *
+# para permitir que hero não precise de um valor padrão
+def create_hero(*, session: Session = Depends(get_session), hero: HeroCreate):
     hashed_password = hash_password(hero.password)
-    with Session(engine) as session:
-        extra_data = {"hashed_password": hashed_password}
-        # o parâmetro upadate está recebendo um dicionário extra que será
-        # incluído e irá atualizar o db_hero que também é um dicionário
-        db_hero = Hero.model_validate(hero, update=extra_data)
-        # breakpoint()
-        session.add(db_hero)
-        session.commit()
-        session.refresh(db_hero)
-        return db_hero
+
+    extra_data = {"hashed_password": hashed_password}
+    # o parâmetro upadate está recebendo um dicionário extra que será
+    # incluído e irá atualizar o db_hero que também é um dicionário
+    db_hero = Hero.model_validate(hero, update=extra_data)
+    # breakpoint()
+    session.add(db_hero)
+    session.commit()
+    session.refresh(db_hero)
+    return db_hero
 
 
 @app.get("/heroes/", response_model=list[HeroPublic])
 # offsete indica que o retorno começa apartir do registro 0,
 # limit limita o retorno padrão em 100 registros
 # le indica qual o numero máximo de retornos limit pode receber
-def read_heroes(offset: int = 0, limit: int = Query(default=100, le=100)):
-    with Session(engine) as session:
-        heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-        return heroes
+def read_heroes(
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+):
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
 
 
-@app.get("/heroes/{hero_id}", response_model=HeroPublic)
-def read_hero(hero_id: int):
-    with Session(engine) as session:
-        hero = session.get(Hero, hero_id)
-        if not hero:
-            raise HTTPException(status_code=404, detail="Hero not found")
-        return hero
+@app.get("/heroes/{hero_id}", response_model=HeroPublicWithTeam)
+def read_hero(*, session: Session = Depends(get_session), hero_id: int):
+    hero = session.get(Hero, hero_id)
+
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
 
 
 @app.patch("/heroes/{hero_id}", response_model=HeroPublic)
-def update_hero(hero_id: int, hero: HeroUpdate):
-    with Session(engine) as session:
-        db_hero = session.get(Hero, hero_id)
+def update_hero(
+    *, session: Session = Depends(get_session), hero_id: int, hero: HeroUpdate
+):
+    db_hero = session.get(Hero, hero_id)
 
-        if not db_hero:
-            raise HTTPException(status_code=404, detail="Hero not found")
+    if not db_hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
 
-        # mode_dump transforma os dados enviados pelo cliente observando
-        # o schema HeroUpdate. Já o parâmetro exclude_unset diz ao Pydantic
-        # para não incluir os valores que não foram enviados pelo cliente
-        hero_data = hero.model_dump(exclude_unset=True)
+    # mode_dump transforma os dados enviados pelo cliente observando
+    # o schema HeroUpdate. Já o parâmetro exclude_unset diz ao Pydantic
+    # para não incluir os valores que não foram enviados pelo cliente
+    hero_data = hero.model_dump(exclude_unset=True)
 
-        extra_data = {}
-        if "password" in hero_data:
-            password = hero_data["password"]
-            hashed_password = hash_password(password)
-            extra_data["hashed_password"] = hashed_password
-        # Para cada um dos campos no objeto do modelo original db_hero,
-        # sqlmodel_update verifica se o campo está disponível no argumento
-        # hero_data e então o atualiza com o valor fornecido
-        # o parâmetro update aqui tem a mesma função do método create_hero
-        db_hero.sqlmodel_update(hero_data, update=extra_data)
-        session.add(db_hero)
-        session.commit()
-        session.refresh(db_hero)
-        return db_hero
+    extra_data = {}
+    if "password" in hero_data:
+        password = hero_data["password"]
+        hashed_password = hash_password(password)
+        extra_data["hashed_password"] = hashed_password
+    # Para cada um dos campos no objeto do modelo original db_hero,
+    # sqlmodel_update verifica se o campo está disponível no argumento
+    # hero_data e então o atualiza com o valor fornecido
+    # o parâmetro update aqui tem a mesma função do método create_hero
+    db_hero.sqlmodel_update(hero_data, update=extra_data)
+    session.add(db_hero)
+    session.commit()
+    session.refresh(db_hero)
+    return db_hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(*, session: Session = Depends(get_session), hero_id: int):
+    hero = session.get(Hero, hero_id)
+
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
+
+
+@app.post("/teams", response_model=TeamPublic)
+def create_team(*, session: Session = Depends(get_session), team: TeamCreate):
+    db_team = Team.model_validate(team)
+    session.add(db_team)
+    session.commit()
+    session.refresh(db_team)
+    return db_team
+
+
+@app.get("/teams/", response_model=list[TeamPublic])
+def read_teams(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+):
+    teams = session.exec(select(Team).offset(offset).limit(limit)).all()
+    return teams
+
+
+@app.get("/teams/{team_id}", response_model=TeamPublicWithHeroes)
+def read_team(*, team_id: int, session: Session = Depends(get_session)):
+    team = session.get(Team, team_id)
+
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+
+    return team
+
+
+@app.patch("/teams/{team_id}", response_model=TeamPublic)
+def update_team(
+    *, session: Session = Depends(get_session), team_id: int, team: TeamUpdate
+):
+    db_team = session.get(Team, team_id)
+
+    if not db_team:
+        raise HTTPException(status_code=404, detail="Team not found")
+
+    team_data = team.model_dump(exclude_unset=True)
+
+    for key, value in team_data.items():
+        setattr(db_team, key, value)
+
+    session.add(db_team)
+    session.commit()
+    session.refresh(db_team)
+
+    return db_team
+
+
+@app.delete("/teams/{team_id}")
+def delete_team(*, session: Session = Depends(get_session), team_id: int):
+    team = session.get(Team, team_id)
+
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+
+    session.delete(team)
+    session.commit()
+
+    return {"ok": True}
